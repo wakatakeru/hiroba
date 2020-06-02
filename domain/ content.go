8d50893114a21ea87691ddfdf6750ae16521a7c0
wakatakeru <s19g460@stu.kagawa-u.ac.jp>
2020-06-02 22:34:50 +0900

    rename

--- domain/ content.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Content struct {
-	ID     int    `json:"id"`
-	SiteID int    `json:"site_id"`
-	UserID int    `json:"user_id"`
-	Title  string `json:"title"`
-	Body   string `json:"body"`
-}
--- domain/content.go
+++ domain/content.go
@@ -1,7 +1,9 @@
 package domain
 
 type Content struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
+	ID     int    `json:"id"`
+	SiteID int    `json:"site_id"`
+	UserID int    `json:"user_id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
 }
